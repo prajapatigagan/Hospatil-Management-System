8542aae07c8203c0e3e89d7dfa207821c10787ee
prajapatigagan <gaganprajapati121@gmail.com>
2026-02-25 23:23:22 +0530

    first commit

---
+++ src/main/java/com/gagan/Hospatil_Management_System/HospatilManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gagan.Hospatil_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospatilManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospatilManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gagan/Hospatil_Management_System/entity/patient.java
@@ -0,0 +1,10 @@
+package com.gagan.Hospatil_Management_System.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class patient {
+    @Id
+    private Long id;
+}
